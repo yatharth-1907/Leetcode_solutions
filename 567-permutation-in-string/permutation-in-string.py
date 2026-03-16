1c601cd28bada7775bdef65d711147fe3c43bbef
Yatharth Patankar <yatharthpatankar07@gmail.com>
2026-03-16 10:39:09 +0530

    Time: 18 ms (62.98%) | Memory: 19.4 MB (79.41%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,12 +1,20 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1=sorted(s1)
-        i=0
-        j=len(s1)-1
-        while j<len(s2):
-            per= sorted(s2[i:j+1])
-            if per==s1:
-                return True 
-            i+=1
-            j+=1
-        return False
+       s1_dict= [0]*26
+       s2_dict= [0]*26
+       for letter in s1:
+           s1_dict[ord(letter)-ord('a')]+=1
+       i=0
+       j=0
+
+       while j<len(s2):
+           s2_dict[ord(s2[j])-ord('a')]+=1
+           if j-i+1==len(s1):
+               if s1_dict==s2_dict:
+                   return True
+               else:
+                   s2_dict[ord(s2[i])-ord('a')]-=1
+                   i+=1
+           j+=1
+
+       return False
